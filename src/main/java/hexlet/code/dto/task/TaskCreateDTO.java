eb9f3d04e97e25c304da3df8afd4cfc812662262
maxtiish <cearness@mail.ru>
2024-08-04 01:22:33 +0700

    fix dto

--- src/main/java/hexlet/code/dto/task/TaskCreateDTO.java
+++ src/main/java/hexlet/code/dto/task/TaskCreateDTO.java
@@ -1,6 +1,7 @@
 package hexlet.code.dto.task;
 
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
@@ -11,12 +12,11 @@ import java.util.Set;
 @Getter
 @Setter
 public class TaskCreateDTO {
-    @NotBlank
     private Long assigneeId;
 
     private Integer index;
 
-    @NotBlank
+    @NotNull
     private String status;
 
     @Size(min = 1)

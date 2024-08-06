3fe3adc0685871d360588639b268c9f001b3647c
maxtiish <cearness@mail.ru>
2024-08-06 16:25:02 +0700

    fix fix

--- src/main/java/hexlet/code/dto/task/TaskCreateDTO.java
+++ src/main/java/hexlet/code/dto/task/TaskCreateDTO.java
@@ -3,7 +3,6 @@ package hexlet.code.dto.task;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
 

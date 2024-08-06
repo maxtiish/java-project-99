03c6666339bd00c6540b25da4ce91062f5a227a8
maxtiish <cearness@mail.ru>
2024-08-06 16:09:08 +0700

    add createdAt

--- src/main/java/hexlet/code/dto/task/TaskCreateDTO.java
+++ src/main/java/hexlet/code/dto/task/TaskCreateDTO.java
@@ -3,9 +3,11 @@ package hexlet.code.dto.task;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.time.LocalDate;
 import java.util.Set;
 
 @Getter
@@ -20,9 +22,12 @@ public class TaskCreateDTO {
     private String status;
 
     @NotBlank
+    @Size(min = 1)
     private String title; //name
 
     private String content; //description
 
     private Set<Long> taskLabelIds;
+
+    private LocalDate createdAt;
 }
